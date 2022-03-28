06aecb84327171f8c251e94a226296b393a6370d
Vadym <zinchenkovadym@gmail.com>
2022-03-28 20:53:19 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -4,21 +4,18 @@ import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Projects from "./components/projects/Projects";
 import Contacts from "./components/contacts/Contacts";
-import { UseEffectScroll } from 'react-use-smooth-scroll'
 import Footer from "./components/footer/Footer";
 
 function App() {
 
   return (
     <div className="App">
-        <UseEffectScroll>
             <Header/>
             <Home/>
             <About/>
             <Projects/>
             <Contacts/>
             <Footer/>
-        </UseEffectScroll>
     </div>
   );
 }
